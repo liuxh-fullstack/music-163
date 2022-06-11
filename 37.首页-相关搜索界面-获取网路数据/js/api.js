8101de1ff05f730c9f92b148352d780e6f79f3fc
liuxiaohui <18567918203liu@gmail.com>
2022-06-11 14:37:29 +0800

    相关搜索界面-获取网路数据

---
+++ 37.首页-相关搜索界面-获取网路数据/js/api.js
@@ -0,0 +1,70 @@
+; (function () {
+    // 服务器地址
+    axios.defaults.baseURL = 'http://localhost:3000'
+    // 超时时间
+    axios.defaults.timeout = 3000
+
+    class NJHttp {
+        static get(url = "", data = {}) {
+            return new Promise(function (resolve, reject) {
+                axios.get(url, {
+                    params: data
+                }).then(function (response) {
+                    resolve(response.data)
+                }).catch(function (error) {
+                    reject(error)
+                })
+            })
+        }
+
+        static post(url = "", data = {}) {
+            return new Promise(function (resolve, reject) {
+                axios.post(url, {
+                    params: data
+                }).then(function (response) {
+                    resolve(response.data)
+                }).catch(function (error) {
+                    reject(error)
+                })
+            })
+        }
+    }
+
+    class HomeApis {
+        static getHomeBanner() {
+            return NJHttp.get("/banner", { type: 2 })
+        }
+
+        static getHomeRecommend() {
+            return NJHttp.get("/personalized")
+        }
+
+        static getHomeExclusive() {
+            return NJHttp.get('/personalized/privatecontent')
+        }
+        static getHomeAlbum() {
+            return NJHttp.get('/top/album', {
+                offset: 0,
+                limit: 6
+            })
+        }
+        static getHomeMV() {
+            return NJHttp.get('/personalized/mv')
+        }
+        static getHomeDJ() {
+            return NJHttp.get('/personalized/djprogram')
+        }
+
+        static getHomeHotDetail() {
+            return NJHttp.get("/search/hot/detail")
+        }
+
+        static getHomeSearchsuggest(keywords){
+            return NJHttp.get("/search/suggest?keywords="+keywords+"&type=mobile")
+        }
+    }
+
+    window.NJHttp = NJHttp
+    window.HomeApis = HomeApis
+})()
+
